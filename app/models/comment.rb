64a9818a043b64ca5cad9153e90c038ef1d532de
Reizka <reizka.deepone@gmail.com>
2023-11-20 10:08:05 +0100

    comments to users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   include Visible
 
   belongs_to :article
+  belongs_to :user
   #validates :title, presence: true
   #validates :body, presence: true, length: { minimum: 2 }
 end
