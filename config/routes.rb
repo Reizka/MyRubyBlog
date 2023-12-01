32190182bf471069fa037615b2f729edde6399ce
Renny <reizka.deepone@gmail.com>
2023-12-01 08:40:04 +0100

    latest changes - adming dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
     get 'admin/articles'
     get 'admin/comments'
     get 'admin/users'
-    get 'admin/show_articles'
+    get 'admin/show_article/:id', to:'admin#show_article', as: 'admin_article'
   end
-  get 'search', to:'search#index' 
+  get 'search', to:'search#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
